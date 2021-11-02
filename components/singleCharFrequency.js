74f8eb7e0631f8b7aa706227920e00d7a2539aff
SKR301 <chunnu.bihat@gmail.com>
2021-11-02 17:57:22 +0530

    Single character frequency #3 backend done

--- components/singleCharFrequency.js
+++ components/singleCharFrequency.js
@@ -9,18 +9,21 @@ class SingleCharFrequency extends Component {
     }
 
     componentDidUpdate = () => {
-        var string = this.props.fileData;
+        var string = this.props.fileData.replace(/\s/g, '');
         var charFrequency = new Map();
         string.split('').forEach(function(ch) {
             charFrequency.has(ch) ? charFrequency.set(ch, charFrequency.get(ch)+1) : charFrequency.set(ch, 1) ;
         });
         
+        //sorting map by value
         charFrequency = [...charFrequency.entries()].sort((a, b) => b[1] - a[1]);
 
         this._plotChart(charFrequency);
     }
 
     _plotChart = (charFrequency) => {
+        console.log(charFrequency);
+
         var chart = am4core.create("chartdiv", am4charts.XYChart);
         charFrequency.forEach(function (element) {
             chart.data.push({
@@ -40,14 +43,18 @@ class SingleCharFrequency extends Component {
         series.dataFields.valueY = "count";
         series.dataFields.categoryX = "character";
         series.name = "Single Character Frequency Count";
+
+        series.columns.template.tooltipText = "\n({categoryX},{valueY})";
+        chart.tooltip.background.fill = am4core.color("#67b7dc");
+        chart.tooltip.label.fontSize = 20;
+        chart.tooltip.label.textAlign = "middle";
     }
 
     render = () => {
         return (
             <div>
                 <h1>Single char frequency</h1>
-                <p>{this.props.fileData}</p>
-                <div id="chartdiv" style={{flex: 1}}></div>
+                <div id="chartdiv" style={{ width: 1500, height: 500}}></div>
             </div>
         );
     }
