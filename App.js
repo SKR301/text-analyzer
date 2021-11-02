24ace10c73b1aff00d9d3809993cf4923d24de43
SKR301 <chunnu.bihat@gmail.com>
2021-11-02 23:37:21 +0530

    started working on routes

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import SelectFile from './components/selectFile';
 import SingleCharFrequency from './components/singleCharFrequency';
+import DoubleCharFrequency from './pages/doubleCharFrequency';
+import VowelVSConsonant from './pages/vowelVSconsonant';
+import { Route, Link, BrowserRouter, Redirect } from 'react-router-dom'
 
 class App extends Component {
     constructor(props) {
@@ -10,15 +13,25 @@ class App extends Component {
 
     getFileData = (data) => {
         this.setState({fileData: data});
+        console.log(this.state.fileData);
+        return <Redirect to="/singleCharFreq"/>
     }
 
     render = () => {
         return (
-            <div>
-                <SelectFile sendFileData = {this.getFileData}/>
-                <SingleCharFrequency fileData = {this.state.fileData}/>
-            </div>
-            
+            // <div>
+            //     <SelectFile sendFileData = {this.getFileData}/>
+            //     <SingleCharFrequency fileData = {this.state.fileData}/>
+            // </div>
+            < BrowserRouter >
+                <div>
+                    {/* <Route exact path="/" component={SelectFile} sendFileData ={this.getFileData}/> */}
+                    <Route exact path="/" component = {() => (<SelectFile sendFileData ={this.getFileData}/>)}/>
+                    <Route path="/singleCharFreq" component={SingleCharFrequency} fileData = {this.state.fileData}/>
+                    <Route path="/doubleCharFreq" component={DoubleCharFrequency}/>
+                    <Route path="/VowelVSConsonant" component={VowelVSConsonant}/>
+                </div>
+         </ BrowserRouter >
         );
     }
 }
---
+++ pages/singleCharFrequency.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SingleCharFrequency extends Component {
+
+    render = () => {
+        return (
+            <div>
+                <h1>Single char frequency</h1>
+            </div>
+        );
+    }
+}
+
+export default SingleCharFrequency;
