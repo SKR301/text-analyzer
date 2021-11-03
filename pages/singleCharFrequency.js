07e5a0c28b30ac63ece8cb31cc05d0bb84dd02e4
SKR301 <chunnu.bihat@gmail.com>
2021-11-03 23:25:33 +0530

    single character frequency with routing implemented(POC) #6 #3

--- pages/singleCharFrequency.js
+++ pages/singleCharFrequency.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
+import BarChart from '../components/barChart';
 
 class SingleCharFrequency extends Component {
     constructor(props) {
         super(props);
         this.state = this.props.location.state;
-        console.log(this.state);
+        this.getCharFrequency = this.getCharFrequency.bind();
     }
 
     componentDidMount = () => {
-        console.log("mount");
+        var charFrequency = this.getCharFrequency(this.state.fileData.replace(/\s/g, ''));
+        new BarChart().plotChart(charFrequency);
     }
 
-    componentDidUpdate = () => {
-        console.log("update");
+    getCharFrequency(string){
+        var charFrequency = new Map();
+        string.split('').forEach(function(ch) {
+            charFrequency.has(ch) ? charFrequency.set(ch, charFrequency.get(ch)+1) : charFrequency.set(ch, 1) ;
+        });
+
+        return charFrequency;
     }
+
+    // componentDidUpdate = () => {
+    //     console.log("update");
+    // }
+
 
     render = () => {
         return (
             <div>
                 <h1>Single char frequency</h1>
+                <BarChart />
             </div>
         );
     }
